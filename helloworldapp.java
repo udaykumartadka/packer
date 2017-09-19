90a9e88996f4b45bcbb39386c3091358c248de90
ooha-m <omanduri@sysgain.com>
2017-09-19 14:28:35 +0530

    Delete helloworldapp.java

--- helloworldapp.java
+++
@@ -1,7 +0,0 @@
-import java.util.Scanner;
-public class HelloWorld {
-   public static void main(String[] args) {
-      // Prints "Hello, World" in the terminal window.
-      System.out.println("Hello, World");
-   }
-}
